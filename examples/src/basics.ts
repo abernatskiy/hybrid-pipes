d14ac8e76f4027021df9bed6038029f7ebc815be
Anton Bernatskiy (kohama) <abernatskiy@toha.li>
2025-09-24 06:19:35 +0900

    Working basic example
    	modified:   examples/package.json
    	modified:   examples/src/basics.ts

--- examples/src/basics.ts
+++ examples/src/basics.ts
@@ -2,35 +2,35 @@ import {
   createEvmPortalSource,
   createTarget,
   EvmQueryBuilder
-} from '@sqd-pipes/core'
+} from '@abernatskiy/hybrid-pipes-core'
 
 async function main() {
 
   const queryBuilder = new EvmQueryBuilder()
-  queryBuilder.addFields({
-    block: {
-      // These two fields are required.
-      // For now please manually add them to the query builder.
-      // Once is enough, anywhere is fine.
-      number: true, hash: true,
-    },
-    log: {
-      address: true,
-      topics: true,
-      data: true,
-      transactionHash: true,
-    }
-  })
-  queryBuilder.addLog({
-    request: {
-      address: ['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'], // USDC
-      topic0: ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'], // Transfer
-    },
-    range: {
-      from: 20000000,
-      to: 20000000,
-    }
-  })
+    .addFields({
+      block: {
+        // These two fields are required.
+        // For now please manually add them to the query builder.
+        // Once is enough, anywhere is fine.
+        number: true, hash: true,
+      },
+      log: {
+        address: true,
+        topics: true,
+        data: true,
+        transactionHash: true,
+      }
+    })
+    .addLog({
+      request: {
+        address: ['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'], // USDC
+        topic0: ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'], // Transfer
+      },
+      range: {
+        from: 20000000,
+        to: 20000000,
+      }
+    })
 
   const source = createEvmPortalSource({
     portal: 'https://portal.sqd.dev/datasets/ethereum-mainnet',
